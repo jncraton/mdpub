aa14a1ab4cb73bb5253e1cdcda45a7934e451bbc
Jon Craton <jncraton@gmail.com>
2025-08-06 21:26:43 -0400

    Create top-level functions

--- mdpub/__main__.py
+++ mdpub/__main__.py
@@ -3,9 +3,6 @@ import mistletoe
 import re
 from ebooklib import epub
 
-filename = sys.argv[1]
-output_filename = sys.argv[2]
-
 
 def get_title(doc):
     # First heading becomes title
@@ -26,50 +23,57 @@ def get_top_heading(doc):
     return top_level
 
 
-with open(filename, "r") as fin:
-    doc = mistletoe.Document(fin)
+def create_epub(filename, output_filename):
+    with open(filename, "r") as fin:
+        doc = mistletoe.Document(fin)
 
-    top_level = get_top_heading(doc)
-    title = get_title(doc)
+        top_level = get_top_heading(doc)
+        title = get_title(doc)
 
-    with mistletoe.HtmlRenderer() as renderer:
-        rendered = renderer.render(doc)
+        with mistletoe.HtmlRenderer() as renderer:
+            rendered = renderer.render(doc)
 
-# Split by second highest heading, prepend content we split by
-wedge = f"\n<h{top_level+1}"
-chapters = rendered.split(wedge)
-chapters = [wedge + c for c in chapters]
-chapters[0] = chapters[0][len(wedge) :]
+    # Split by second highest heading, prepend content we split by
+    wedge = f"\n<h{top_level+1}"
+    chapters = rendered.split(wedge)
+    chapters = [wedge + c for c in chapters]
+    chapters[0] = chapters[0][len(wedge) :]
 
-book = epub.EpubBook()
+    book = epub.EpubBook()
 
-# set metadata
-book.set_identifier("isbn")
-book.set_title("title")
-book.set_language("en")
+    # set metadata
+    book.set_identifier("isbn")
+    book.set_title("title")
+    book.set_language("en")
 
-book.add_author("author")
+    book.add_author("author")
 
-# create chapters
+    # create chapters
 
-toc = []
-book.spine = ["nav"]
+    toc = []
+    book.spine = ["nav"]
 
-for i, chapter in enumerate(chapters):
-    first_line = chapter.strip().split("\n")[0]
-    chapter_title = re.sub(r"<.+?>", "", first_line)
+    for i, chapter in enumerate(chapters):
+        first_line = chapter.strip().split("\n")[0]
+        chapter_title = re.sub(r"<.+?>", "", first_line)
 
-    c = epub.EpubHtml(title=chapter_title, file_name=f"{i}.xhtml", lang="en")
-    c.content = chapter
-    book.add_item(c)
-    book.spine.append(c)
-    toc.append(c)
+        c = epub.EpubHtml(title=chapter_title, file_name=f"{i}.xhtml", lang="en")
+        c.content = chapter
+        book.add_item(c)
+        book.spine.append(c)
+        toc.append(c)
 
-# define Table Of Contents
-book.toc = tuple(toc)
+    # define Table Of Contents
+    book.toc = tuple(toc)
 
-# add default NCX
-book.add_item(epub.EpubNcx())
+    # add default NCX
+    book.add_item(epub.EpubNcx())
 
-# write to the file
-epub.write_epub(output_filename, book, {})
+    # write to the file
+    epub.write_epub(output_filename, book, {})
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    output_filename = sys.argv[2]
+    create_epub(filename, output_filename)
